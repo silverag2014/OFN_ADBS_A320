a3a9b1e4ed7d082428278c34d35a0f2696fb61ed
unknown <YY@yuanyin1.lenovo.com>
2014-12-19 18:55:17 +0800

    add .h file

---
+++ ADBS_A320.h
@@ -0,0 +1,64 @@
+/*
+    ADBSA320.h
+	DECEMBER 4, 2010
+	by: SILVER
+*/
+
+#ifndef _ADBS_A320_h_
+#define _ADBS_A320_h_
+
+
+
+// A320 Register Defines
+#define	Product_ID	0x00
+#define	Revision_ID	0x01
+#define	Motion		0x02
+#define	Delta_X		0x03
+#define	Delta_Y		0x04
+#define	SQUAL		0x05
+#define	Shutter_Upper	0x06
+#define	Shutter_Lower	0x07
+#define	Maximum_Pixel	0x08
+#define	Pixel_Sun	0x09
+#define	Minimum_Pixel	0x0a
+#define	Pixel_Grab	0x0b
+#define	CRC0		0x0c
+#define	CRC1		0x0d
+#define	CRC2		0x0e
+#define	CRC3		0x0f
+
+#define	Self_Test	0x10
+#define	Configuration_Bits	0x11
+#define	LED_Control		0x1a
+#define	IO_Mode			0x1c
+#define	Motion_Control		0x1d
+#define	Observation		0x2e
+#define	Soft_RESET		0x3a
+#define	Shutter_Max_Hi		0x3b
+#define	Shutter__Max_Lo		0x3c
+#define	Inverse_Revision_ID	0x3e
+#define	Inverse_Product_ID	0x3f
+#define	OFN_Engine		0x60
+#define	OFN_Resolution		0x62
+#define	OFN_Speed_Control	0x63
+#define	OFN_Speed_ST12		0x64
+#define	OFN_Speed_ST21		0x65
+#define	OFN_Speed_ST23		0x66
+#define	OFN_Speed_ST32		0x67
+#define	OFN_Speed_ST34		0x68
+#define	OFN_Speed_ST43		0x69
+#define	OFN_Speed_ST45		0x6a
+#define	OFN_Speed_ST54		0x6b
+#define	OFN_AD_CTRL		0x6d
+#define	OFN_AD_ATH_HIGH		0x6e
+#define	OFN_AD_DTH_HIGH		0x6f
+#define	OFN_AD_ATH_LOW		0x70
+#define	OFN_AD_DTH_LOW		0x71
+#define	OFN_Quantize_CTRL	0x73
+#define	OFN_XYQ_THRESH		0x74
+#define	OFN_FPD_CTRL		0x75
+#define	OFN_Orientation_CTRL	0x77
+
+#endif
+
+
